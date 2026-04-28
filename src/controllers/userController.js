f8d24027bc022d2e4c559790bfcdc3dcad384df2
user.name "choudhry64 <git config --global user.name "choudhry64>
2026-04-28 18:42:36 +0530

    feat: add login and Api with jwt token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,27 +1,59 @@
 const UserModel = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  try {  
-  const { firstName, lastName, email, password, role, username } = req.body;
+  try {
+    const { firstName, lastName, email, password, role, username } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  await UserModel.create({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: hashedPassword,
-    role: role,
-    username : username
-  });
+    await UserModel.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword,
+      role: role,
+      username: username,
+    });
 
-  res.status(201).json({
+    res.status(201).json({
       message: "register completed",
     });
-    } catch(error){
-        res.status(500).json({message : error.message})
-    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-module.exports = register;
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({
+      email: email,
+    });
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (user && passwordMatch) {
+      const token = jwt.sign(
+        {
+          userId: user._id,
+        },
+        process.env.JWT_SECRET,
+      );
+
+      res.json({
+        token,
+      });
+    } else {
+      res.status(403).json({
+        message: "incorrect cred",
+      });
+    }
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { register, login };
